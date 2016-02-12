5900c6a2cba886c4c25c47a94ed2659448ef3059
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-12 22:52:29 +0100

    Generate cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'site-backuppctypo3org'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures site-backuppctypo3org'
+long_description 'Installs/Configures site-backuppctypo3org'
+version '0.1.0'
